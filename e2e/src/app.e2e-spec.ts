6c002ef7071ab8724f1bf42d926b309a22875978
sergeihanka <sergeiahanka@gmail.com>
2019-02-24 17:11:19 -0600

    init

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to product-app!');
+    expect(page.getParagraphText()).toEqual('Welcome to incentify-app!');
   });
 });
