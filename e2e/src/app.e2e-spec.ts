c27c318167d8fff9e0fa52406e90e58b21cee79e
Sergei Hanka <sergeiahanka@gmail.com>
2019-03-24 20:59:26 -0500

    name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to perks4me-app!');
+    expect(page.getParagraphText()).toEqual('Welcome to perk4me-app!');
   });
 });
