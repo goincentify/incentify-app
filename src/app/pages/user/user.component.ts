5e8e28ad008c93b1359a3077f5f714ab6738f358
Sergei Hanka <sergeiahanka@gmail.com>
2019-03-26 19:53:02 -0600

    added icons to project for quick loading, user page loading

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -9,18 +9,45 @@ import { UserService } from '@app/service';
 @Component({
   selector: 'app-root',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
   displayedColumns = ['id', 'username', 'salary', 'age'];
   dataSource = new MatTableDataSource<User>();
   currentUser: User;
+  navOptions = navOptions;
+
+  name;
+  image;
+  userOnTimeGoal;
+  userTotalDaysGoal;
+  userTier: string;
+  userPoints: number;
+  userOnTimePercent: number;
+  userTardyPercent: number;
+  userOnTimePercentile: number;
+  userTotalDays: number;
+  userDayStreakPercentile: number;
+  userDaysWorkedPercentile: number;
+  userDayStreak: number;
 
   constructor(private userService: UserService, private route: ActivatedRoute, private authService: AuthService) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
-      this.currentUser = data.user;
+
+      let user = data.user;
+
+      this.name = user.firstName + " " + user.lastName;
+      this.userDayStreakPercentile = user.daystreak_percentile;
+      this.userDaysWorkedPercentile = user.totaldays_percentile;
+      this.userOnTimePercentile = user.ontimepercent_percentile;
+      this.userOnTimePercent = user.ontimedays / user.totaldays * 100;
+      this.userTardyPercent = 1 - this.userOnTimePercent;
+      this.userTotalDays = user.totaldays;
+      this.userPoints = user.points;
+      this.userTier = user.tier;
+      this.userDayStreak = user.daystreak;
     });
   } 
 
@@ -29,4 +56,43 @@ export class UserComponent implements OnInit {
   }
 
 }
+
+let navOptions = [
+  {
+    icon: "flight",
+    option: "Travel",
+    description: "Redeem rewards points towards a vacation",
+    url: "marketplace/travel"
+  },
+  {
+    icon: "card_giftcard",
+    option: "Gift Cards",
+    description: "Turn those rewards points into credit at your favorite store",
+    url: "marketplace/gift card"
+  },
+  {
+    icon: "fitness_center",
+    option: " Fitness",
+    description: "Reward your body with rewards points",
+    url: "marketplace/fitness"
+  },
+  {
+    icon: "shopping_basket",
+    option: "Electronics",
+    description: "Reward yourself with new gadgets",
+    url: "marketplace/electronics"
+  },
+  {
+    icon: "directions_car",
+    option: "Transportation",
+    description: "Give your rewards some miles",
+    url: "marketplace/transportation"
+  },
+  {
+    icon: "attach_money",
+    option: "Cash Out",
+    description: "Redeem your points for a VISA gift card.",
+    url: "/userprofile/redeem",
+  }
+]
 
